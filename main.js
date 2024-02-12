29af71776acc4d25b9f1fab4484c47c1d8009c45
joonhyung park <joonharry706@gmail.com>
2024-02-12 22:43:45 +0900

    second commit

--- main.js
+++ main.js
@@ -9,74 +9,87 @@
 // 유저가 이미 입력한 숫자를 또 입력하면, 이미 입력한 숫자라고 알려주며 기회를 깎지 않는다.
 
 
-let computerNum = 0
-let playButton = document.getElementById("play-button")
-let userInput = document.getElementById("user-input")
-let resultArea = document.getElementById("result-area")
-let resetButton = document.getElementById("reset-button")
-let chances = 5
-let gameOver = false
-let chanceArea = document.getElementById("chance-area")
-let history = []
+// 필요한 html elements 다 가져오기
+let computerNumber = 0;
+let playButton = document.getElementById("play-button");
+let resetButton = document.querySelector(".button-reset");
+let userInput = document.querySelector("#user-input");
+let resultAreaImg = document.querySelector(".main-img");
+let resultText = document.querySelector(".result-text");
+let chanceArea = document.getElementById("chance-area");
+let gameOver = false;
+let chances = 5; // 남은 기회
+let userValueList = []; // 유저가 입력한 숫자들 리스트
 
-playButton.addEventListener("click", play)
-resetButton.addEventListener("click", reset)
-userInput.addEventListener("focus", function(){userInput.value=""})
-//focus 이벤트에만 쓰이는 함수이므로 따로 함수 선언 후 불러오지 않고 바로 익명함수를 써서 메모리 절약을 하였다. 
-function pickRandomNum(){
-    computerNum = Math.floor(Math.random() * 100)+1;
-    console.log("정답", computerNum )
+chanceArea.innerHTML = `남은 기회:${chances}`;
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", focusInput);
+
+function pickRandomNumber() {
+  // 랜덤숫자 뽑기
+
+  computerNumber = Math.floor(Math.random() * 100) + 1;
+  console.log("정답", computerNumber);
 }
 
-function play(){
-    let userValue = userInput.value;
+function play() {
+  // 숫자 추측하기
+  const userValue = userInput.value;
+  if (userValue < 1 || userValue > 100) {
+    resultText.textContent = "1부터 100 사이의 숫자를 입력 해주세요";
 
-    if(userValue<1 || userValue>100){
-        resultArea.textContent = "1과 100사이 숫자를 입력해 주세요" 
-        //1과 100사이 범위의 숫자를 입력하지 않았을때 표시됨
-        return; //어떠한 것도 return하지 않고 종료하므로 밑에 실행문들은 실행되지 않는다.
-    }
+    return;
+  }
 
-    if(history.includes(userValue)){ 
-        // includes 함수는 중복된 값이 있는지 판별하는 함수이다.
-        resultArea.textContent = "이미 입력한 숫자입니다! 다른 숫자를 입력해 주세요~"
-        return; //chance가 깎이지 않고 그대로 종료되게 만든다.
-    }
-    chances --;
-    chanceArea.textContent = `남은기회:${chances}번`; //(``백틱은 동적인 값, ""는 정적인 값에 사용)
-    console.log("chance", chances)
-    if(userValue < computerNum){
-        resultArea.textContent = "Up!!!"
-    }else if(userValue > computerNum){
-        resultArea.textContent = "DOWN!!!"
-    }else {
-        resultArea.textContent = "맞추셨습니다!!!"
-        gameOver=true
-    }
+  if (userValueList.includes(userValue)) {
+    resultText.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요";
 
-    history.push(userValue)
-    console.log(history)
+    return;
+  }
 
-    if(chances < 1){
-        gameOver=true
-    }
-    if (gameOver == true){
-        playButton.disabled = true;
-    }
+  chances--;
+  chanceArea.innerHTML = `남은 기회:${chances}`;
+  userValueList.push(userValue);
+  if (userValue < computerNumber) {
+    resultAreaImg.src =
+      "https://media0.giphy.com/media/3ov9jExd1Qbwecoqsg/200.gif";
+    resultText.textContent = "Up!";
+  } else if (userValue > computerNumber) {
+    resultAreaImg.src = "https://media.giphy.com/media/r2puuhrnjG7vy/giphy.gif";
+    resultText.textContent = "Down!";
+  } else {
+    resultAreaImg.src =
+      "https://media.tenor.com/images/0a81b89954678ebe228e15e35044f7a5/tenor.gif";
+    resultText.textContent = "쿠폰번호: 2ZBJE45A";
+    gameOver = true;
+  }
 
+  if (chances == 0) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+function focusInput() {
+  userInput.value = "";
 }
 
 function reset() {
-    //user input 창이 깨끗하게 정리되고
-    userInput.value = ""
-    //새로운 번호가 생성되고
-    pickRandomNum();
-    resultArea.textContent = "결과값이 여기 나옵니다"
-    chances = 5
-    chanceArea.textContent = "남은기회:5번"; 
-    playButton.disabled= false;
-    gameOver=false;
-    history = []
+  //리셋
+  pickRandomNumber();
+  userInput.value = "";
+  resultAreaImg.src =
+    "https://cdn.imweb.me/thumbnail/20220609/36e49c247b509.gif";
+  resultText.textContent = "맞추면 입장권 할인 쿠폰 증정!";
+  gameOver = false;
+  playButton.disabled = false;
+  chances = 5;
+  chanceArea.innerHTML = `남은 기회:${chances}`;
+  userValueList = [];
 }
 
-pickRandomNum()
+pickRandomNumber();
